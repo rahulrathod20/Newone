c07aec087ca60d6b6a26255fd488be16cbbb1a66
rahulrathod20 <104188895+rahulrathod20@users.noreply.github.com>
2022-04-26 12:28:35 +0530

    edited

--- Github1/src/task1/Demo.java
+++ Github1/src/task1/Demo.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo 
 
-{
+{       public void m1()
+      {     
+	System.out.println("hi");
+       }
 
 	public static void main(String[] args) {
 		
